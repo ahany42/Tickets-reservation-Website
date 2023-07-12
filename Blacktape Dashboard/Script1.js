b74b88ca678fbee36e842c21f7c8445c6e6145f9
abdelrahmn594 <abdelrahmanemad999@gmail.com>
2023-07-13 01:34:39 +0300

    tickets pending

--- Blacktape Dashboard/Script1.js
+++ Blacktape Dashboard/Script1.js
@@ -5,3 +5,19 @@ let nav = document.querySelector(".navcontainer");
 menuicn.addEventListener("click", () => {
     nav.classList.toggle("navclose");
 }) */
+
+
+
+var moreinfo=document.getElementById("moreinfo");
+var expandinfo=document.getElementById("expandinfo");
+moreinfo.addEventListener('click', function(){
+ 
+    if(expandinfo.style.display=='none' || expandinfo.style.display ==""){
+        expandinfo.style.display='grid';
+    }
+    else{
+        expandinfo.style.display='none';
+    }
+
+
+});
