eee0aee74d2e23c7b36269c9284f78f9e542e10a
RamyNasser4 <ramy_rnz@hotmail.com>
2023-07-12 03:45:40 +0300

    finished upcoming and past pages

--- Blacktape Dashboard/Upcomingscreen.js
+++ Blacktape Dashboard/Upcomingscreen.js
@@ -2,6 +2,8 @@ var add =document.getElementById("add");
 var del = document.getElementById("delete");
 var edit = document.getElementById("edit");
 var addform = document.getElementById("Addform");
+var deleteform = document.getElementById("Deleteform");
+var editform = document.getElementById("Editform");
 var forms = document.getElementsByClassName("form");
 add.addEventListener('click',function(){
     if(addform.style.display == "flex"){
@@ -15,6 +17,40 @@ add.addEventListener('click',function(){
         addform.style.display = "flex";
         for(var i=0;forms.length>i;++i){
             if(forms[i]!=addform){
+                forms[i].style.display = "none";
+            }
+        }
+    }
+})
+del.addEventListener('click',function(){
+    if(deleteform.style.display == "flex"){
+        deleteform.style.display = "none";
+        for(var i=0;forms.length>i;++i){
+            if(forms[i]!=deleteform){
+                forms[i].style.display = "none";
+            }
+        }
+    }else{
+        deleteform.style.display = "flex";
+        for(var i=0;forms.length>i;++i){
+            if(forms[i]!=deleteform){
+                forms[i].style.display = "none";
+            }
+        }
+    }
+})
+edit.addEventListener('click',function(){
+    if(editform.style.display == "flex"){
+        editform.style.display = "none";
+        for(var i=0;forms.length>i;++i){
+            if(forms[i]!=editform){
+                forms[i].style.display = "none";
+            }
+        }
+    }else{
+        editform.style.display = "flex";
+        for(var i=0;forms.length>i;++i){
+            if(forms[i]!=editform){
                 forms[i].style.display = "none";
             }
         }
